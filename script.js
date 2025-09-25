24fb7cb0ff83dacc4891d8fbba8a653acf9c5267
akbar-batti <aliakbatti@gmail.com>
2025-09-25 19:20:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const drawer = document.querySelector('.drawer');
 
 menuToggle.addEventListener('click', () => {
   drawer.classList.toggle('open');
+  document.body.classList.toggle('drawer-open');
 });
